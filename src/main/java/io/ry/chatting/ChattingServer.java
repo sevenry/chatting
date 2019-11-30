2976e8d3904bd0d7bc757e1fe25d47001d20917a
sevenry <835266995@qq.com>
2019-11-30 15:08:08 +0800

    add login logic

--- src/main/java/io/ry/chatting/ChattingServer.java
+++ src/main/java/io/ry/chatting/ChattingServer.java
@@ -4,14 +4,15 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.logging.Logger;
-import io.ry.chatting.helloworld.GreeterGrpc;
-import io.ry.chatting.helloworld.HelloRequest;
-import io.ry.chatting.helloworld.HelloReply;
+import io.ry.chatting.ChatServiceGrpc;
+import io.ry.chatting.LoginReply;
+import io.ry.chatting.LoginRequest;
 
-public class HelloWorldServer {
-  private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
-
+public class ChattingServer {
+  private static final Logger logger = Logger.getLogger(ChattingServer.class.getName());
+  private static HashSet<String> userList = new HashSet<String>();
   private Server server;
 
   private void start() throws IOException {
@@ -27,7 +28,7 @@ public class HelloWorldServer {
       public void run() {
         // Use stderr here since the logger may have been reset by its JVM shutdown hook.
         System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        HelloWorldServer.this.stop();
+        ChattingServer.this.stop();
         System.err.println("*** server shut down");
       }
     });
@@ -52,24 +53,22 @@ public class HelloWorldServer {
    * Main launches the server from the command line.
    */
   public static void main(String[] args) throws IOException, InterruptedException {
-    final HelloWorldServer server = new HelloWorldServer();
+    final ChattingServer server = new ChattingServer();
     server.start();
     server.blockUntilShutdown();
   }
 
-  static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+  static class GreeterImpl extends ChatServiceGrpc.ChatServiceImplBase {
 
     @Override
-    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-      HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+    public void login(LoginRequest req, StreamObserver<LoginReply> responseObserver) {
+      String user = req.getUser();
+      boolean contain = userList.contains(user);
+      userList.add(user);
+      LoginReply reply = LoginReply.newBuilder().setSuccess(!contain).build();
       responseObserver.onNext(reply);
       responseObserver.onCompleted();
     }
-    @Override
-    public void sayHelloAgain(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-      HelloReply reply = HelloReply.newBuilder().setMessage("Hello again " + req.getName()).build();
-      responseObserver.onNext(reply);
-      responseObserver.onCompleted();
-    }
+
   }
 }
